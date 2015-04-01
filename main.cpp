d9a33f3f31a78a1af987e08064bcdde8f6d16683
Saqib Kayani <m.s.kayani@edu.salford.ac.uk>
2015-04-01 19:54:07 +0100

    added a comment.

--- main.cpp
+++ main.cpp
@@ -1,12 +1,37 @@
 #include <QApplication>
 #include <QQmlApplicationEngine>
 
+// other inlcudes...
+#include <QDirIterator>
+#include <QDir>
+#include <QStringList>
+#include <QDebug>
+#include <QQuickView>
+#include <QObject>
+#include <QQuickItem>
+
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
 
     QQmlApplicationEngine engine;
     engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
+
+    //adding a comment.
+    QDir mDir;
+
+    foreach(QFileInfo mItm, mDir.drives())
+    {
+        //qDebug() << mItm.absoluteFilePath();
+        QDirIterator it(mItm.absoluteFilePath(), QStringList() << "*.jpg", QDir::Files, QDirIterator::Subdirectories);
+        mItm.filePath();
+
+                while (it.hasNext()) {
+                    qDebug() << "Directory: " << it.next();
+                }
+        //printf(mItm.absoluteFilePath());
+    }
+
 
     return app.exec();
 }
